4f75560828a331e26f548c7e59d25502b2a8d2e4
Abrar Hasnat <hasnat23105101269@diu.edu.bd>
2026-09-21 11:40:17 +0600

    Database table create

--- database/blog_app.sql
+++ database/blog_app.sql
@@ -1,1 +1,29 @@
 CREATE DATABASE blog_app;
+use blog_app;
+
+create table users (
+	id int auto_increment primary key,
+    name varchar(200) NOT NULL,
+    email varchar(200) not null unique,
+    password varchar(200) not null,
+    create_at TIMESTAMP default CURRENT_TIMESTAMP
+);
+
+create table posts(
+	id int auto_increment primary key,
+    titel varchar(200) not null,
+    content text not null,
+    author_id int not null,
+    create_at TIMESTAMP default CURRENT_TIMESTAMP,
+    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+create table comments(
+	id int auto_increment primary key,
+    text TEXT not null,
+    post_id int not null,
+    author_id int not null,
+    create_at TIMESTAMP default CURRENT_TIMESTAMP,
+    foreign key (post_id) references posts(id) on delete cascade,
+    foreign key (author_id) references users(id) on delete cascade
+);
